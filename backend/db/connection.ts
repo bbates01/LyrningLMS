706417ec5383dbdf744cf163f242df9ae4272406
Jacob Woodward <jgw96@byu.edu>
2026-03-25 17:29:27 -0600

    Students can now see accurate  grades

--- backend/db/connection.ts
+++ backend/db/connection.ts
@@ -1,7 +1,12 @@
 import dotenv from 'dotenv';
 import { Pool } from 'pg';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-dotenv.config();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config({ path: path.join(__dirname, '..', '.env') });
 
 const connectionString = process.env.DATABASE_URL;
 
